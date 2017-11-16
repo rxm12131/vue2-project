b5e53103153b6b547a2222ea30c60dbd12db0a63
rxm12131 <625772057@qq.com>
2017-11-16 19:58:31 +0800

    douyu

---
+++ src/pages/RoomList.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+    <div class="mr-root">
+        <back>
+            <p class="title">{{$route.params.ganmeName}}</p>
+        </back>
+        <div class="mr-content">
+            <div class="room-info" v-for="(r,index) in rooms" :key="index">
+                <img :src="r.room_src" alt="">
+                <div class="room-title">
+                    <div class="room-name">{{r.room_name}}</div>
+                    <div class="nickname">
+                        <i class="icon-user"></i>
+                        <span>{{ r.nickname}}</span>
+                    </div>
+                    <div class="online">
+                        <i class="icon-group"></i>
+                        <span>{{ r.online | number}}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="load-more" v-if="!showLoad">
+                <span @click="loadMore">加载更多</span>
+            </div>
+        </div>
+        <back-top></back-top>
+        <loading v-if="showLoad"></loading>
+        <p v-if="error">网络请求失败，请稍候再试</p>
+    </div>
+</template>
+
+<script>
+import Back from '../components/Back'
+import Loading from '../components/Loading'
+import BackTop from '../components/BackTop'
+export default{
+    data(){
+        return{
+            rooms:[],
+            error:false,
+            showLoad:true,
+            page:0
+        }
+    },
+    components:{
+        BackTop,Back,Loading
+    },
+    created(){
+        this.getInfo(this.page)
+    },
+    methods:{
+         getInfo(page){
+            this.$http.get(`/douyuapi/RoomApi/live/${this.$route.params.gameId}?offset=${page}$limit=20`)
+            .then(res =>{
+                this.error =false
+                this.rooms = this.rooms.concat(res.data.data)
+                setTimeout(() =>{
+                    this.showLoad =false
+                },2000)
+            })
+            .catch(err =>{
+                this.error = true
+                this.showLoad = false
+            })
+        },
+        loadMore(){
+            this.page++
+            this.getInfo(this.page)
+        }
+    }
+}
+</script>
